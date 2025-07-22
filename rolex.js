07722e952b235d48d3bb59f5257680125e7bfea1
VETRIVEL R <vetrivelpep@gmail.com>
2025-07-22 22:56:09 +0530

    Create rolex.js

---
+++ rolex.js
@@ -0,0 +1,8 @@
+function showWatches() {
+    document.getElementById('watches').scrollIntoView({ behavior: 'smooth' });
+}
+
+function buyNow(watchName) {
+    alert(`You have selected the ${watchName}. Proceed to checkout!`);
+}
+
